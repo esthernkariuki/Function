c4d707188304503f48695e826c9250f924bbaa3d
esther <esthernnyamburaa@gmail.com>
2025-04-08 11:00:10 +0300

    javascript functions assignments

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // reverse all the elements in the string in ascending order and the specific elements in descending order
 function reversedstr(arr) {
-    return arr.map(str => str.split('').reverse().join('')).sort();
-  }
-  
+    return arr.map(str => str.split('').reverse().join('')).sort();}
   function specificItem(arr, indices) {
     return arr.map((str, index) => {
       if (indices.includes(index)) {
-        return str.split('').reverse().join('');
-      }
+        return str.split('').reverse().join('');}
       return str;
-    }).sort((a, b) => a.b);
-  }
-  
+    }).sort((a, b) => a.b);}
+    
   let strs = ["Thinkpad", "X1 Carbon", "Yoga", "IdeaPad"];
   console.log(reversedstr(strs));
   console.log(specificItem(strs, [1 ,2]));
@@ -21,10 +17,7 @@ function reversedstr(arr) {
     return arr.map(num => {
       if (num > 0) return 'positive';
       if (num < 0) return 'negative';
-      return 'zero';
-    });
-  }
-  
+      return 'zero';});}
   let num= [-2,-90,10,5,35];
   console.log(spotNum(num));
 
@@ -33,9 +26,7 @@ function reversedstr(arr) {
 // Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
   function numMultiplied2(arr) {
     arr.forEach(num => {
-      console.log(num * 2);
-    });
-  }
+      console.log(num * 2);});}
   let nums = [1,2,3];
   numMultiplied2(nums);
 
@@ -44,24 +35,17 @@ function reversedstr(arr) {
 // Given an array of objects, where each object represents an employee with an id, name, and salary property, write a function that returns a new array of employee objects sorted by their salary in ascending order.
 
 function sortedsalary(employees) {
-  return employees.sort((a, b) => a - b);
-}
-
+  return employees.sort((a, b) => a - b);}
 let employees = [
   { id: 4, name: 'Esther', salary: 500 },
-  { id: 8, name: 'James', salary: 600},
-  
-];
+  { id: 8, name: 'James', salary: 600},];
 console.log(sortedsalary(employees));
 
 // Write a function that takes in an array of numbers and consoles the first four items multiplied by 8 and the last two added by 5. Console the array with the new values
 function specificNumberss(arr) {
     arr.slice(0, 4).forEach(num => console.log(num * 8));
-    arr.slice(-2).forEach(num => console.log(num + 5));
-  }
-  
-
-  let numArray = [11, 20, 16,54, 12, 36];
+    arr.slice(-2).forEach(num => console.log(num + 5));}
+    let numArray = [11, 20, 16,54, 12, 36];
   specificNumberss(numArray);
 
 
